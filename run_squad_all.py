40feedf1d8df7baba7935e17bf4c23c4375e0138
Xiaojie Jin <xjin6@uncc.edu>
2025-04-16 14:03:12 -0400

    Update run_squad_all.py

--- run_squad_all.py
+++ run_squad_all.py
@@ -3,7 +3,7 @@
 
 import os
 import logging
-from squad_new import Squad
+from squad import Squad
 
 # 配置日志
 logging.basicConfig(
@@ -16,7 +16,7 @@ def main():
     """主函数"""
     try:
         # 设置数据路径
-        data_path = "/fs/ess/PAS1475/Xiaojie/spatialQC/test_data/visiumhd_data"
+        data_path = "/fs/ess/PAS1475/Xiaojie/spatialQC/test_data/RCC_TMA542_section05_v132_spatialdata"
         output_path = "/fs/ess/PAS1475/Xiaojie/spatialQC/test_data"
         
         # 检查数据目录是否存在
@@ -25,12 +25,15 @@ def main():
         
         logger.info(f"开始处理Xenium数据: {data_path}")
         
-        # 创建Squad对象并读取数据
-        # squad = Squad.from_cosmx(
-        #     common_path=data_path,
-        #     fov_number=12,
-        #     file_prefix = "RCC_BPC_23_tma542"
-        # )
+        #创建Squad对象并读取数据
+        squad = Squad.from_cosmx(
+            common_path=data_path,
+            fov_number=12,
+            file_prefix = "RCC_BPC_23_tma542",
+            # bin_size = 20, 
+            # ratio_neighbors = 2, 
+            # extend_ratio = 0.2
+        )
         # squad = Squad.from_merscope(
         #     path=data_path,
         #     fov_number=0
@@ -38,10 +41,11 @@ def main():
         # squad = Squad.from_xenium(
         #     path=data_path,
         # )
-        squad = Squad.from_visiumhd(
-            path = data_path,
-            dataset_id = "Visium_HD_Human_Colon_Cancer_P1"
-        )
+        # squad = Squad.from_visiumhd(
+        #     path = data_path,
+        #     dataset_id = "Visium_HD_Human_Colon_Cancer_P1"
+        # )
+
         # # 1. 输出unique的region值
         # logger.info("输出unique的region值...")
         # unique_regions = squad.sdata.points["transcripts"]["fov_name"].unique()
@@ -51,13 +55,13 @@ def main():
         # unique_regions = squad.sdata.table.obs["region"].unique()
         # logger.info(f"Unique regions: {unique_regions}")
         
-        # 2. 保存基因名到txt文件
-        logger.info("保存基因名到文件...")
-        gene_names_file = os.path.join(output_path, "gene_names.txt")
-        with open(gene_names_file, 'w') as f:
-            for gene in squad.sdata.table.var_names:
-                f.write(f"{gene}\n")
-        logger.info(f"基因名已保存到: {gene_names_file}")
+        # # 2. 保存基因名到txt文件
+        # logger.info("保存基因名到文件...")
+        # gene_names_file = os.path.join(output_path, "gene_names.txt")
+        # with open(gene_names_file, 'w') as f:
+        #     for gene in squad.sdata.table.var_names:
+        #         f.write(f"{gene}\n")
+        # logger.info(f"基因名已保存到: {gene_names_file}")
         
         # # 3. 保存transcripts信息到csv文件
         # logger.info("保存transcripts信息...")
